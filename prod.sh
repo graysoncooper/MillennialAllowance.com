3fb4c1614bc36e50491ddbfafb2eb67a849e043f
Shane Burkhart <shaneburkhart@gmail.com>
2017-02-27 21:25:45 -0600

    Don't remove images in deploy.

--- prod.sh
+++ prod.sh
@@ -6,7 +6,3 @@ docker-compose -f docker-compose.prod.yml down
 # Clean up docker stopped docker containers
 docker rm $(docker ps -aq)
 docker-compose -f docker-compose.prod.yml up -d
-
-# Remove untagged docker images
-echo "============ Cleanup Docker Images =============="
-docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}") || true
